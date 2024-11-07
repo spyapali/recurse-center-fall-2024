a67ad33f15ea32af2b415af26ebe570f7effd30e
Shalini Pyapali <shalu.pyapali@gmail.com>
2024-11-07 13:30:43 -0500

    Make changes to the scrolling behavior!

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -1,42 +1,25 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import './App.css';
+import useGifSearch from './useGifSearch';
+
+const LIMIT = 50
 
 function App() {
-  const [images, setImages] = useState(true);
   const [paginationOffset, setPaginationOffset] = useState(0);
+  const observer = useRef()
 
+  const { images, hasMore, loading, error } = useGifSearch(paginationOffset)
 
-  const loadImages = useCallback(async () => {
-    const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?q=home&api_key=ukwRl9rXa8ROYBQMW1EGPkyZdIY4HQ31&offset=${paginationOffset}`
-    );
-    const data = await response.json()
-    setImages(data.data)
-    console.log({ data: data.data })
-    setPaginationOffset(data.pagination.offset)
-  }, [paginationOffset])
-
-  useEffect(() => {
-    loadImages()
-  }, [loadImages])
-
-  const handleScroll = useCallback(() => {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    const windowHeight = window.innerHeight;
-    const docHeight = document.documentElement.scrollHeight;
-
-    if (scrollTop + windowHeight >= docHeight - 2) {
-      // User has scrolled to the bottom
-      console.log('Reached the bottom!');
-      // Call a function to load more images
-      loadImages()
-
-    };
-  }, [loadImages])
-
-  useEffect(() => {
-    handleScroll()
-  }, [handleScroll]);
+  const lastImageElementRef = useCallback(node => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(images => {
+      if (images[0].isIntersecting && hasMore) {
+        setPaginationOffset(prevPaginationOffset => prevPaginationOffset + LIMIT)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [hasMore, loading])
 
 
   return (
@@ -45,9 +28,17 @@ function App() {
         <div className="text-center mb-4 text-4xl text-white font-bold">Home Sweet Home 🏠</div>
         <div className="columns-4">
           {images.map((image, index) => {
-            return <img className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={index} src={image.images.original.url} />
+            console.log("hey")
+            if (images.length !== index + 1) {
+              return <img className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={image} src={image} />
+            } else {
+              return <img ref={lastImageElementRef} className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={image} src={image} />
+
+            }
           })}
         </div>
+        <div>{loading && "Loading..."}</div>
+        <div>{error && "Error..."}</div>
       </div>
     </div>
   );
--- giphy/src/useGifSearch.js
+++ giphy/src/useGifSearch.js
@@ -1,18 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 export default function useGifSearch(offset) {
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(null)
     const [images, setImages] = useState([])
     const [loading, setLoading] = useState(true);
+    const [hasMore, setHasMore] = useState(false);
 
     useEffect(() => {
+        console.log("I'm triggering!")
+        setLoading(true)
+        setError(false)
         axios({
             method: 'GET',
             url: "https://api.giphy.com/v1/gifs/search",
-            params: { q: "home", offset: offset, api_key: "ukwRl9rXa8ROYBQMW1EGPkyZdIY4HQ31" }
+            params: { q: "home", offset, api_key: "ukwRl9rXa8ROYBQMW1EGPkyZdIY4HQ31" }
         }).then((res) => {
-            console.log(res.data)
+            setImages(images => {
+                return [...new Set([...images, ...res.data.data.map((image) => image.images.original.url)])]
+            })
+            setHasMore(res.data.data.length > 0)
+            setLoading(false)
+        }).catch((e) => {
+            setError(e.message)
         })
     }, [offset])
+    console.log({ images })
+    return { error, images, loading, hasMore }
 }
