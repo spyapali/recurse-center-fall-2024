b75a3b30a169861f32bf4656878b7ea3e4ea7940
Shalini Pyapali <shalu.pyapali@gmail.com>
2024-11-08 16:01:55 -0500

    Add a magnifying glass as a "search" icon to the search bar.

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -1,39 +1,44 @@
 import { useState, useRef, useCallback } from 'react';
 import './App.css';
 import useGifSearch from './useGifSearch';
+import { ReactComponent as MagnifyingGlass } from "./magnifying-glass.svg"
 
 const LIMIT = 50
 
 function App() {
   const [paginationOffset, setPaginationOffset] = useState(0);
+  const [content, setContent] = useState("");
   const observer = useRef()
 
-  const { images, hasMore, loading, error } = useGifSearch(paginationOffset)
+  const { images, loading, error } = useGifSearch(content, paginationOffset)
 
   const lastImageElementRef = useCallback(node => {
     if (loading) return;
     if (observer.current) observer.current.disconnect()
     observer.current = new IntersectionObserver(images => {
-      if (images[0].isIntersecting && hasMore) {
+      if (images[0].isIntersecting) {
         setPaginationOffset(prevPaginationOffset => prevPaginationOffset + LIMIT)
       }
     })
     if (node) observer.current.observe(node)
-  }, [hasMore, loading])
+  }, [loading])
 
 
   return (
     <div className="bg-black p-20">
-      <div className="max-w-screen-2xl mx-auto">
-        <div className="text-center mb-4 text-4xl text-white font-bold">Home Sweet Home 🏠</div>
+      <div className="mx-auto w-fit">
+        {/* <div className="text-center mb-8 text-4xl text-white font-bold">Home Sweet Home 🏠</div> */}
+
+        <div className="flex">
+          <input className="rounded-l-lg text-center text-2xl w-[150px] md:w-[500px] lg:w-[1000px] text-white font-bold" />
+          <button className="p-5 rounded-r-lg border-x-from-purple-500 bg-gradient-to-r from-purple-500 to-pink-500"><MagnifyingGlass className="w-14 h-14" /></button>
+        </div>
         <div className="columns-4">
           {images.map((image, index) => {
-            console.log("hey")
-            if (images.length !== index + 1) {
-              return <img className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={image} src={image} />
-            } else {
+            if (images.length === index + 1) {
               return <img ref={lastImageElementRef} className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={image} src={image} />
-
+            } else {
+              return <img className="border-yellow-600 mb-2 border-2 rounded-md" alt="home" width={400} key={image} src={image} />
             }
           })}
         </div>
--- giphy/src/useGifSearch.js
+++ giphy/src/useGifSearch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-export default function useGifSearch(offset) {
+export default function useGifSearch(content, offset) {
     const [error, setError] = useState(null)
     const [images, setImages] = useState([])
     const [loading, setLoading] = useState(true);
@@ -14,7 +14,7 @@ export default function useGifSearch(offset) {
         axios({
             method: 'GET',
             url: "https://api.giphy.com/v1/gifs/search",
-            params: { q: "home", offset, api_key: "ukwRl9rXa8ROYBQMW1EGPkyZdIY4HQ31" }
+            params: { q: content, offset, api_key: "ukwRl9rXa8ROYBQMW1EGPkyZdIY4HQ31" }
         }).then((res) => {
             setImages(images => {
                 return [...new Set([...images, ...res.data.data.map((image) => image.images.original.url)])]
@@ -24,7 +24,6 @@ export default function useGifSearch(offset) {
         }).catch((e) => {
             setError(e.message)
         })
-    }, [offset])
-    console.log({ images })
+    }, [content, offset])
     return { error, images, loading, hasMore }
 }
